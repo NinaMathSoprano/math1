375fd346556d3c16427cb6c1738eb190bfadbf25
Nina Bukreeva <Ineya566@gmail.com>
2024-11-09 17:36:57 +0300

    другие типы данных

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package com.itproger;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        Scanner scan = new Scanner(System.in);
+
+        //System.out.println("Введите ваше имя");
+        //String username = scan.nextLine();
+        // Вызвали класс сканер
+        //Ввели переменную типа из класса сканер
+        //Попросили пользователя ввести имя
+        //&#x421;&#x43E;&#x445;&#x440;&#x430;&#x43D;&#x438;&#x43B;&#x438; &#x43A;&#x430;&#x43A; &#x43F;&#x435;&#x440;&#x435;&#x43C;&#x435;&#x43D;&#x43D;&#x443;&#x44E; &#x435;&#x433;&#x43E; &#x438;&#x43C;&#x44F; &#x438; &#x432;&#x44B;&#x432;&#x435;&#x43B;&#x438;
+        //System.out.println("Привет, "+ username);
+        int num1 = scan.nextInt();
+        byte num2 = scan.nextByte();
+        boolean b = scan.hasNextBoolean();
+        float num3 = scan.nextFloat();
+        // в зависимости от типа данных, котрый мы хотим получить, мы мжем использовать разные методы
+
+        }
+}
